2e38a62bc7a06693353c531b95b4c9a7a60f02a1
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2024-11-22 13:56:46 -0300

    feat: create DTO EventRequestDTO

--- src/main/java/com/eventostec/api/domain/event/EventRequestDTO.java
+++ src/main/java/com/eventostec/api/domain/event/EventRequestDTO.java
@@ -1,2 +1,16 @@
-package com.eventostec.api.domain.event;public class EventRequestDTO {
+package com.eventostec.api.domain.event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public record EventRequestDTO (
+    String title,
+    String description,
+    Long date,
+    String city,
+    String state,
+    Boolean remote,
+    String eventUrl,
+    MultipartFile image
+){
 }
