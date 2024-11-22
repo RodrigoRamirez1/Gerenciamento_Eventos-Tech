973384576a4d5afb0157f63292e22cc1ccf7b504
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2024-11-22 13:57:55 -0300

    feat: create migration the table event

--- src/main/resources/db/migration/V1__create-table-event.sql
+++ src/main/resources/db/migration/V1__create-table-event.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE evento (
+CREATE TABLE event (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
