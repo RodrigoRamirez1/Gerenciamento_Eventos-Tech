0a1a8ba7fd7d7f5591efa8e3b29c0b94783852fb
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2024-11-26 10:53:15 -0300

    feat: create method uploadImg and convertMultipartToFile.

--- src/main/java/com/eventostec/api/services/EventService.java
+++ src/main/java/com/eventostec/api/services/EventService.java
@@ -1,14 +1,27 @@
 package com.eventostec.api.services;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
 
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
@@ -27,6 +40,25 @@ public class EventService {
     }
 
     private String uploadImg(MultipartFile multipartFile){
-        return "";
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try {
+            File file = this.convertMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, filename, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, filename).toString();
+        } catch (Exception e) {
+            System.out.println("erro ao subir arquivo");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
     }
 }
